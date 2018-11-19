aed356aee45701ee4e9efc3ac63da83bb01e2818
Antti Kissaniemi <antti@resq-club.com>
2018-11-19 16:39:34 +0200

    Add a simple Button

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'es5',
+	jsxBracketSameLine: true,
+	printWidth: 100,
+}
---
+++ src/Button.vue
@@ -0,0 +1,45 @@
+<!--
+	A simple button component to test out importing .vue components
+
+	<Button></Button-test>
+-->
+
+<template>
+	<button @click="clicked"><slot /></button>
+</template>
+
+<script>
+export default {
+	methods: {
+		clicked() {
+			this.$emit('click')
+			// console.log('clicked')
+		}
+	}
+};
+</script>
+
+<style lang='stylus' scoped>
+button {
+	border: 1px solid #888
+	height: 32px
+	padding: 10px
+	line-height: 14px
+	border-radius: 16px
+	font-size: 14px
+	text-transform: uppercase
+	font-variant: small-caps
+
+	outline: none;
+
+	&:hover {
+		background: black
+		color: white
+	}
+
+	&:focus {
+		border-color: black
+	}
+}
+
+</style>
