a78729877693c94b330f0de9fc90572b03c6486d
Marko Rintamäki <marko.rintamaki@gmail.com>
2018-12-10 17:38:44 +0200

    dismissableMixin

--- index.mjs
+++ index.mjs
@@ -1,3 +1,7 @@
 import Button from './src/Button.vue'
+import dismissableMixin from './src/mixins/dismissableMixin'
 
-export { Button }
+export {
+    Button,
+    dismissableMixin
+}
---
+++ src/mixins/dismissableMixin.js
@@ -0,0 +1,21 @@
+export default {
+    beforeDestroy() {
+        this.$el.remove()
+        window.removeEventListener('click', this.dismissableMixin_destroy)
+        window.removeEventListener('keydown', this.dismissableMixin_escDestroyer)
+    },
+    mounted() {
+        this.dismissableMixin_destroy = () => {
+            this.$destroy()
+        }
+        this.dismissableMixin_escDestroyer = (e) => {
+            if (e.which === 27) { // esc
+                this.$destroy()
+            }
+        }
+        setTimeout(() => {
+            window.addEventListener('click', this.dismissableMixin_destroy)
+            window.addEventListener('keydown', this.dismissableMixin_escDestroyer)
+        }, 10)
+    }
+}
