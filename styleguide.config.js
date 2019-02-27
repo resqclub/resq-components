453afe018795996235bbebc45ba17b82712fe903
Antti Kissaniemi <antti@resq-club.com>
2019-02-27 22:13:21 +0200

    Add global styleguide.css with background color for Table

--- styleguide.config.js
+++ styleguide.config.js
@@ -5,6 +5,7 @@ module.exports = {
 	skipComponentsWithoutExample: true,
 	require: [
 		'vue-material-design-icons/styles.css',
+		path.join(__dirname, 'styleguide.css'),
 		// Load all Brother 1816 fonts
 		path.join(__dirname, 'src/fonts/brother-1816.css'),
 		path.join(__dirname, 'src/styleguide.js')
