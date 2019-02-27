8a463f17933e23673b64c824614bbd0f194d1e50
Antti Kissaniemi <antti@resq-club.com>
2019-02-27 22:27:01 +0200

    Export Table

--- index.mjs
+++ index.mjs
@@ -1,7 +1,9 @@
 import Button from './src/components/Button.vue'
 import Lang from './src/components/Lang.vue'
+import Table from './src/components/Table.vue'
 
 export {
 	Button,
-	Lang
+	Lang,
+	Table
 }
